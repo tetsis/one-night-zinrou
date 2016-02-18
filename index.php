e3b1460dd1554e01bd0eaad58780fbb797bd82d4
tetsis <hitoshi.zin.nakao@gmail.com>
2016-02-18 15:55:30 +0900

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>仁狼 -Zinrou-</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h1>仁狼 -Zinrou-</h1>
+
+</body>
+</html>
