664d11cc55d5946a22fc75bc05a65ba14dfb2bcc
tetsis <hitoshi.zin.nakao@gmail.com>
2016-07-09 11:02:07 +0900

    [fix] Lobbyで表示する村がないときの処理を修正1

--- villageManagement.php
+++ villageManagement.php
@@ -138,6 +138,8 @@ class VillageManagement {
     public function clickUpdate($socket) {
         outputLog('ENTER clickUpdate');
         $this->updateVillageList($socket);
+        $txData = json_encode(array('type'=>'system', 'state'=>LOBBY, 'message'=>'display'));
+        sendMessage($txData, $socket);
     }
 
     //socketで「決定」をクリック
