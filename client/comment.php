46e7ff77bb3fc8dab48e7c01de56949de1287282
tetsis <hitoshi.zin.nakao@gmail.com>
2016-09-19 11:45:03 +0900

    [add] コメントフォームに名前欄を追加

--- client/comment.php
+++ client/comment.php
@@ -15,8 +15,9 @@
     if ($_POST[btn_commentSubmit]) {
         error_log("COMMENT: ". $_POST[txa_comment]. "\n", 3, '/var/log/zinrou.log');
         $slackApiKey = 'SLACK_API_KEY';
+        $name = $_POST['txt_name'];
         $text = $_POST['txa_comment'];
-        $text = urlencode($text);
+        $text = urlencode($name. ": ". $text);
         $url = "https://slack.com/api/chat.postMessage?token=${slackApiKey}&channel=%23one-night-zinrou&text=${text}&as_user=true";
         file_get_contents($url);
         print "<div class='header'>";
@@ -26,9 +27,11 @@
     }
 ?>
 <div class="header">
-    開発者へのコメントを書いてください
 </div>
 <form action = 'comment.php' method = 'post'>
+    <div class = "header">あなたの名前（必須ではありませんが、開発者があなたのコメントを反映したリリース情報をお伝えするのに役立ちます。）</div>
+    <input type="text" maxlength="20" name="txt_name" class="txt">
+    <div class = "header">開発者へのコメントを書いてください</div>
     <textarea name="txa_comment" class='txt'></textarea>
     <input type = 'submit' name = 'btn_commentSubmit' value = '送信' class='btn_main'/>
 </form>
